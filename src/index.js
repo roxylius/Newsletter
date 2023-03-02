05ba1252565439a8cefd5c97040a9e13c5862868
roxylius <98657313+roxylius@users.noreply.github.com>
2023-03-02 22:27:28 +0530

    change package json homepage and add build and deploy

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from "react";
 // import { ReactDOM } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter as Router } from "react-router-dom";
 import App from './app';
 
 
 const root = createRoot(document.getElementById('root'));
 
-root.render(<BrowserRouter><App /></BrowserRouter>);
+root.render(<Router><App /></Router>);
